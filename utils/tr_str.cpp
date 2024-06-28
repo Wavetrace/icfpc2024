18b752779e7bdfb8bf8375b375bf56a562594697
Peter <kerzum@google.com>
2024-06-28 20:15:27 +0200

    added string encode (with -e flag)

--- utils/tr_str.cpp
+++ utils/tr_str.cpp
@@ -1,17 +1,32 @@
+// recode string from stdin to stdout.
+// by default decode the string
+// with "-e" flag encode it
+
 #include <iostream>
 #include <string>
 #include <cassert>
 #include <stdexcept>
 using namespace std;
 
-string in  = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-string out = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n";
+const string in  = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+const string out = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n";
 
-int main(int argc, char** argv) {
+void recode(bool decode) {
+    const string* inp = &::in;
+    const string* outp = &::out;
+
+    // if we encode, swap the tables
+    if (!decode)
+        swap(inp, outp);
+
+    const string& in = *inp;
+    const string& out = *outp;
+
     while (char c = cin.get()) {
-        if (cin.eof() || cin.fail() || c == 0)
+        if (cin.eof())
             break;
-        if (c == '\n') {
+        // if we encode, encode \n as well
+        if (c == '\n' && decode) {
             cout << endl;
             continue;
         }
@@ -23,5 +38,15 @@ int main(int argc, char** argv) {
         }
         cout << out[idx];
     }
-    // cout << endl;
 }
+
+int main(int argc, char** argv) {
+    bool decode = true;
+    if (argc > 1 && argv[1] == string("-e"))
+        decode = false;
+
+    recode(decode);
+    // for convenience; maybe remove
+    cout << endl;
+}
+
