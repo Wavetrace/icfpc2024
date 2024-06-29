e493e6263960db6812d3bbaf13120f17b4a61488
Peter <kerzum@google.com>
2024-06-29 05:13:06 +0200

    seemingly working decoder

---
+++ code/common.h
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <sstream>
+// #include <vector>
+#include <string>
+#include <stdexcept>
+#include <algorithm>
+
+#include <cassert>
+
+using namespace std;
+
+ostream& dlog = cerr;
+
+
+class error: public runtime_error {
+private:
+    string Msg;
+
+public:
+    error(string s = "error: ")
+        : runtime_error("unused"), Msg(s)
+    {}
+    /* override */ const char* what() const noexcept {
+        return Msg.c_str();
+    }
+
+    template <class T> error& operator<<(const T& m) {
+        ostringstream os;
+        os << m;
+        Msg += os.str();
+        return *this;
+    }
+};
+#define NI throw error(string() + __FUNCTION__  + " - not implemented")
+
--- code/decode.cpp
+++ code/decode.cpp
@@ -1,58 +1,4 @@
-#include <iostream>
-#include <sstream>
-// #include <vector>
-#include <string>
-#include <stdexcept>
-#include <algorithm>
-
-#include <cassert>
-
-using namespace std;
-
-ostream& dlog = cerr;
-
-
-class error: public runtime_error {
-private:
-    string Msg;
-
-public:
-    error(string s = "error: ")
-        : runtime_error("unused"), Msg(s)
-    {}
-    /* override */ const char* what() const noexcept {
-        return Msg.c_str();
-    }
-
-    template <class T> error& operator<<(const T& m) {
-        ostringstream os;
-        os << m;
-        Msg += os.str();
-        return *this;
-    }
-};
-#define NI throw error(string() + __FUNCTION__  + " - not implemented")
-
-
-void decodeString(istream& is, ostream& os) {
-    const string in  = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-    const string out = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`|~ \n";
-
-    while (true) {
-        char c = is.get();
-        if (isspace(c)) 
-            return;
-        // todo: consider static map
-        size_t idx = in.find(c);
-        if (idx == string::npos) {
-            cerr << "wrong char: " << int(c) << endl;
-            throw error(string() + "wrong char: " + c);
-        }
-        os << out[idx];
-        if (is.eof())
-            return;
-    }
-}
+#include "common.h"
 
 string decodeString(const string& s) {
     const string in  = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
@@ -60,12 +6,9 @@ string decodeString(const string& s) {
 
     ostringstream os;
     for (char c : s) {
-        // if (isspace(c)) 
-        //    return;
         // todo: consider static map
         size_t idx = in.find(c);
         if (idx == string::npos) {
-            cerr << "wrong char: " << int(c) << endl;
             throw error() << "wrong char: '" << c << "'" << " (" << int(c) <<")";
         }
         os << out[idx];
@@ -78,91 +21,55 @@ string getString(istream& is) {
     is >> s;
 
 
-    if (!is.eof()) {
-        char space;
-        space = is.get();
-        dlog << "str: '" << s << "'" << ", space: " << space << '(' << int(space) << ')' << endl;
-        assert(space == ' ' || is.eof());
-    }
+    char space;
+    space = is.get();
+    // dlog << "str: '" << s << "'" << ", space: " << space << '(' << int(space) << ')' << endl;
+    assert(space == ' ' || is.eof());
 
 
     return decodeString(s);
 }
 
-/*
-void decodeInt(istream& is, ostream& os) {
+int decodeInt(string s) {
     long int val = 0;
-    string s;
-    char space;
-    is >> s; // >> space;
-    // dlog << "str: '" << s << "'" << ", space: " << space << endl;
-    space = is.get();
-    assert(space == ' ');
-    // assert(s.size() > 0);
-    // reverse(s.begin(), s.end());
+
     for (char c : s) {
         int digit = int(c) - int('!');
         // todo: check for overload
         val = val * 93 + digit;
     }
-    dlog << "decoded int: " << val << " ";
-    os << val;
+    return val;
 }
-*/
 
 int getInt(istream& is) {
-    long int val = 0;
     string s;
     is >> s;
 
     char space;
     space = is.get();
-    assert(space == ' ');
+    assert(space == ' ' || is.eof());
 
-    // assert(s.size() > 0);
-    // reverse(s.begin(), s.end());
-    for (char c : s) {
-        int digit = int(c) - int('!');
-        // todo: check for overload
-        val = val * 93 + digit;
-    }
-    // dlog << "decoded int: " << val << " ";
-    return val;
+    return decodeInt(s);
 }
 
-// string decodeWord(string s) {
 void parseToken(istream& is, ostream& os) {
-    // string s;
-    // is >> s;
-
-    // dlog << "word: " << s << endl;
-    // string skip;
-
-    if (is.eof())
-        return;
-
-
     char i = is.get();
     char val = is.peek();
 
     if (is.eof())
         return;
 
-    dlog << "i: " << i << " = " << int(i) << ", v: " << val << ", pos = " << is.tellg() <<  endl;
+    // dlog << "i: " << i << " = " << int(i) << ", v: " << val << ", pos = " << is.tellg() <<  endl;
 
     if (i == ' ')
         throw error("space!");
     if (i == 0)
         throw error("zero!");
 
-    // char i = s.at(0);
-    // string val = s.substr(1);
-
     if (false) {}
     else if (i == 'S') {
         os << '"';
         os << getString(is);
-        // decodeString(is, os);
         os << '"';
     }
     else if (i == 'I')
@@ -181,6 +88,9 @@ void parseToken(istream& is, ostream& os) {
         is.get(); // val
         is.get(); // space
         os << '(';
+
+        // operator in infix
+        // os << ' ' << val << ' ';
         parseToken(is, os);
         os << ' ' << val << ' ';
         parseToken(is, os);
@@ -196,50 +106,43 @@ void parseToken(istream& is, ostream& os) {
         parseToken(is, os);
     }
     else if (i == 'L') {
-        /*
-        ostringstream refs;
-        decodeInt(is, refs); // ref num
-        int ref;
-        istringstream ris(refs.str());
-        ris >> ref;
-        */
-
         int ref = getInt(is);
         os << "( lambda n" << ref << ".";
         parseToken(is, os);
         os << " )";
     }
     else if (i == 'v') {
-        /*
-        ostringstream refs;
-        decodeInt(is, refs); // ref num
-        int ref;
-        istringstream ris(refs.str());
-        ris >> ref;
-        */
-
         int ref = getInt(is);
         os << "n" << ref;
     }
     else {
-        string s;
-        getline(is, s);
-        dlog << "rest: '" << s << "'" << endl;
-        NI << " instr: " << i;
+        is.unget();
+        NI << " instr: '" << i << "'";
     }
 
-    os << ' ';
-    parseToken(is, os);
+    // dlog << "parsed token: " << i << endl;
+    os << ' ' << flush;
 }
 
 
 
 int main() {
-    // throw error("oops");
+    // make tellg work
     string buf;
     getline(cin, buf, char(0));
     istringstream is(buf);
-    // parseToken(cin, cout);
-    parseToken(is, cout);
+
+    try {
+        parseToken(is, cout);
+        if (!is.eof())
+            throw error("input left");
+    } catch(exception& e) {
+        cerr << "error parsing at " << is.tellg() << ": " << e.what() << endl;
+        string s;
+        getline(is, s);
+        // move rest to ex handler
+        dlog << "rest: '" << s << "'" << endl;
+    }
+
 }
 
